5c33c0790589e826f5535625d5cbb4a942cf6dba
Eric Singhartinger <eric@esreverse.com>
2022-08-04 10:07:33 +0200

    Style-Dictionary Installation

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Thu, 04 Aug 2022 07:53:10 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
