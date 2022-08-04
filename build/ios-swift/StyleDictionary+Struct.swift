efb211d83c71d097e13c781aca7674ecd781fabe
Eric Singhartinger <eric@esreverse.com>
2022-08-04 17:26:42 +0200

    husky

--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,14 +4,14 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 04 Aug 2022 15:06:29 GMT
+// Generated on Thu, 04 Aug 2022 15:24:25 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
     internal static let accentDefault = #5a67d8
-    internal static let bgDefault = #f6e05e
+    internal static let bgDefault = #4fd1c5
     internal static let borderRadiusLg = 8
     internal static let borderRadiusSm = 4
     internal static let borderRadiusXl = 16
